3fb30f3bc4c32921f01bd0fe65577d56ab177b93
Tav Hafner <23143092+haftav@users.noreply.github.com>
2022-05-08 11:31:20 -0700

    Adding eslint cache to gitignore

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
+console.log('thing');
+var my_thing = 5;
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
